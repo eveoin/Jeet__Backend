ef9cc99b113a8d055b51fc1f5c2459398cf415b8
jeet <156777031+janjarukiyajeet@users.noreply.github.com>
2024-02-06 10:58:27 +0530

    final run program

--- test.py
+++ test.py
@@ -1,35 +1,23 @@
 import json
-
-import razorpay
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-
-client = razorpay.Client(auth=("rzp_test_BqwXmtR5v1PWNh", "NnS89KiZoIfLw6briAZOnEje"))
+import razorpay
 
 app = Flask(__name__)
 CORS(app)
 
-# Specify the full path to the data.json file
-waiting_list = 'E:\py\demo\data.json'
+# This is the list where the waiting list data is stored
+waiting_list = 'data.json'
 
 try:
     with open(waiting_list, 'r') as file:
         waiting_list = json.load(file)
 except FileNotFoundError:
-    print("Error: File not found.")
-    exit()
-
-if not isinstance(waiting_list, list):
-    print("Error: 'waiting_list' is not a list.")
-    exit()
-
-email_count = sum('Email' in item for item in waiting_list)
-print("Number of Email IDs:", email_count)
+    waiting_list = []
 
 @app.route('/test', methods=['GET'])
 def get_waiting_list():
     return jsonify({'count': len(waiting_list)})
-
 
 @app.route('/test', methods=['POST'])
 def add_to_waiting_list():
@@ -40,11 +28,10 @@ def add_to_waiting_list():
 
     waiting_list.append({
         'Email': data['Email'],
-         #'paymentId': data['paymentId']
+        # 'paymentId': data['paymentId']
     })
 
-
-    return jsonify(waiting_list)
+    return jsonify()
 
 
 if __name__ == '__main__':
