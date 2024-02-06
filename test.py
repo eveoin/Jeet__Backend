1a9b208a4ffda04b7a0ffb89af6a1a14c9ffb13a
jeet <156777031+janjarukiyajeet@users.noreply.github.com>
2024-02-06 15:50:20 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 import json
+import os
+
+import razorpay
+
+client = razorpay.Client(auth=("rzp_test_BqwXmtR5v1PWNh", "NnS89KiZoIfLw6briAZOnEje"))
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import razorpay
 
 app = Flask(__name__)
 CORS(app)
 
-# This is the list where the waiting list data is stored
 waiting_list = 'data.json'
+
 
 try:
     with open(waiting_list, 'r') as file:
-        waiting_list = json.load(file)
-except FileNotFoundError:
+        # Check if the file is not empty
+        file_content = file.read().strip()
+        waiting_list = json.loads(file_content) if file_content else []
+except (FileNotFoundError, json.JSONDecodeError):
     waiting_list = []
+
 
 @app.route('/test', methods=['GET'])
 def get_waiting_list():
     return jsonify({'count': len(waiting_list)})
 
+
 @app.route('/test', methods=['POST'])
 def add_to_waiting_list():
     data = request.get_json()
+
 
     if 'Email' not in data:
         return jsonify({'error': 'Invalid data format'}), 400
@@ -31,7 +41,7 @@ def add_to_waiting_list():
         # 'paymentId': data['paymentId']
     })
 
-    return jsonify()
+    return jsonify(waiting_list)
 
 
 if __name__ == '__main__':
